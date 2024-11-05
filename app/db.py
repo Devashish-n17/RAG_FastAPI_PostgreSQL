94bca712b668c941329b0f343c52e2416040c9f6
Devashish-n17 <devashish.revadkar@nitorinfotech.com>
2024-11-05 11:05:13 +0530

    updating code

--- app/db.py
+++ app/db.py
@@ -4,7 +4,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://practiceDB_owner:RDCok52HPzaE@ep-ancient-art-a54q021a.us-east-2.aws.neon.tech/practiceDB?sslmode=require"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+db_api = os.getenv('DATABASE_URL')
+
+SQLALCHEMY_DATABASE_URL = db_api
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
